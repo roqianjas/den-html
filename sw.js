dc3fb33e731e2eb4859bed6ea63b5b82d60cc298
Roki Anjas <143056983+roqianjas@users.noreply.github.com>
2025-07-26 13:03:31 +0700

    Updates

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'den-website-v1';
+const urlsToCache = [
+  '/',
+  '/src/style.css',
+  '/src/main.js',
+  '/profil.html',
+  '/informasi.html',
+  '/publikasi.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
